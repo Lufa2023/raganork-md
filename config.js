1a884ff5caeb532393b0f24191e777141565d663
Lufa2023 <140992436+Lufa2023@users.noreply.github.com>
2024-07-16 22:17:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const settingsMenu = [
   ]
 DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
-if (!(process.env.SESSION || process.env.SESSION_ID)) throw new Error("Raganork~d74cc29b4e367cba88c6be5ceb676115:6431693439487a5973634d3d")
+if (!(process.env.SESSION || process.env.SESSION_ID)) throw new Error("raganork-md")
 module.exports = {
     VERSION: 'v4.0.0',
     ALIVE: process.env.ALIVE || "https://i.imgur.com/KCnoMM2.jpg Hey {sender}, I'm alive \n Uptime: {uptime}",
